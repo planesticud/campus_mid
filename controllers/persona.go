9edfad3cccc45cb7b3e06e742851b31f033ba57d
Mafe Avendaño <mafe.ud@gmail.com>
2018-05-07 14:25:06 -0500

    PUT de persona

--- controllers/persona.go
+++ controllers/persona.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"encoding/json"
+	"fmt"
 
 	"github.com/astaxie/beego"
 	"github.com/udistrital/campus_mid/models"
@@ -100,6 +101,100 @@ func (c *PersonaController) GuardarPersona() {
 		}
 
 	} else {
+		alerta.Type = "error"
+		alerta.Code = "400"
+		alerta.Body = "ERROR formato incorrecto"
+		c.Data["json"] = alerta
+		c.ServeJSON()
+	}
+
+	c.ServeJSON()
+
+}
+
+// ActualizarPersona ...
+// @Title PutPersona
+// @Description Actualizar Persona
+// @Param	body		body 	models.PersonaDatosBasicos	true		"body for Guardar Persona content"
+// @Success 200 {string} models.Persona.Id
+// @Failure 403 body is empty
+// @router /ActualizarPersona [put]
+func (c *PersonaController) ActualizarPersona() {
+	// persona datos que entran a la funcion ActualizarPersona
+	var persona models.PersonaDatosBasicos
+
+	// alerta que retorna la funcion Actualizar persona
+	var alerta models.Alert
+
+	//acumulado de alertas
+	//var alertas string
+
+	//almacena resultado de la peticion PUT /persona
+	var resultado models.Persona
+
+	//almacena resultado de la peticion PUT /persona_estado_civil
+	//var resultado2 models.PersonaEstadoCivil
+
+	//almacena resultado de la peticion PUT /persona_genero
+	//var resultado3 models.PersonaGenero
+
+	//modelo PersonaGenero
+	//var genero models.PersonaGenero
+
+	//modelo  PersonaEstadoCivil
+	//var estadoCivil models.PersonaEstadoCivil
+
+	//valida que el JSON de entrada sea correcto
+	if err := json.Unmarshal(c.Ctx.Input.RequestBody, &persona); err == nil {
+
+		//funcion que realiza  de la  peticion POST /persona
+		errPersona := request.SendJson("http://"+beego.AppConfig.String("PersonaService")+"/persona", "PUT", &resultado, persona)
+		fmt.Println(errPersona)
+		/*if errPersona == nil && resultado.Id != 0 {
+
+			alertas = alertas + " OK persona "
+			alerta.Type = "OK"
+			alerta.Code = "201"
+
+			estadoCivil.Persona = &resultado
+			estadoCivil.EstadoCivil = persona.EstadoCivil
+
+			//funcion que realiza  de la  peticion POST /persona_estado_civil
+			errEstadoCivil := request.SendJson("http://"+beego.AppConfig.String("PersonaService")+"/persona_estado_civil", "POST", &resultado2, estadoCivil)
+			if errEstadoCivil != nil || resultado2.Id == 0 {
+				alertas = alertas + " ERROR persona_estado_civil "
+				alerta.Type = "error"
+				alerta.Code = "400"
+			} else {
+				alertas = alertas + " OK persona_estado_civil "
+			}
+
+			genero.Persona = &resultado
+			genero.Genero = persona.Genero
+
+			//funcion que realiza  de la  peticion POST /persona_genero
+			errGenero := request.SendJson("http://"+beego.AppConfig.String("PersonaService")+"/persona_genero", "POST", &resultado3, genero)
+			if errGenero != nil || resultado3.Id == 0 {
+				alertas = alertas + " ERROR persona_genero "
+				alerta.Type = "error"
+				alerta.Code = "400"
+			} else {
+				alertas = alertas + " OK persona_genero "
+			}
+
+			alerta.Body = alertas
+			c.Data["json"] = alerta
+			c.ServeJSON()
+		} else {
+			alerta.Type = "error"
+			alerta.Code = "400"
+			alerta.Body = " ERROR persona "
+			c.Data["json"] = alerta
+			c.ServeJSON()
+		}*/
+
+	} else {
+		fmt.Println(err)
 		alerta.Type = "error"
 		alerta.Code = "400"
 		alerta.Body = "ERROR formato incorrecto"
--- routers/commentsRouter_controllers.go
+++ routers/commentsRouter_controllers.go
@@ -9,6 +9,14 @@ func init() {
 
 	beego.GlobalControllerRouter["github.com/udistrital/campus_mid/controllers:PersonaController"] = append(beego.GlobalControllerRouter["github.com/udistrital/campus_mid/controllers:PersonaController"],
 		beego.ControllerComments{
+			Method: "ActualizarPersona",
+			Router: `/ActualizarPersona`,
+			AllowHTTPMethods: []string{"put"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["github.com/udistrital/campus_mid/controllers:PersonaController"] = append(beego.GlobalControllerRouter["github.com/udistrital/campus_mid/controllers:PersonaController"],
+		beego.ControllerComments{
 			Method: "GuardarPersona",
 			Router: `/GuardarPersona`,
 			AllowHTTPMethods: []string{"post"},
